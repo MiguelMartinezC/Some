ab0b91577d001ea15307a2f78fd60d7a54521b68
Miguel Martinez <miguel.martinez@binah.mx>
2011-08-11 20:48:27 -0500

    first commit

---
+++ ex7.rb
@@ -0,0 +1,90 @@
+Last login: Tue Aug  9 06:25:05 on console
+MacBook-Pro-de-MAikOL:~ MAikOL$ ruby ex5.rb
+ruby: No such file or directory -- ex5.rb (LoadError)
+MacBook-Pro-de-MAikOL:~ MAikOL$ cd rubyexample
+-bash: cd: rubyexample: No such file or directory
+MacBook-Pro-de-MAikOL:~ MAikOL$ cd rubyexamples
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ ruby sa.rb
+sadsfgf 250 wqe
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ ruby sa.rb
+Mary had a little lamb.
+Its fleece was whitee as snow.
+And everywhere that Mary went.
+..........
+CheeseBurger
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ CLEAR
+
+
+
+
+
+
+
+
+
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ ruby sa.rb
+Mary had a little lamb.
+Its fleece was whitee as snow.
+And everywhere that Mary went.
+..........
+CheeseBurger
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ ruby sa.rb
+Mary had a little lamb.
+Its fleece was whitee as snow.
+And everywhere that Mary went.
+..........
+CheeseBurgerMacBook-Pro-de-MAikOL:rubyexamples MAikOL$ ruby sa.rb
+Mary had a little lamb.
+Its fleece was whitee as snow.
+And everywhere that Mary went.
+..........
+CheeseBurgerMacBook-Pro-de-MAikOL:rubyexamples MAikOL$ clear
+
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ clear
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ ruby sa.rb
+Mary had a little lamb.
+Its fleece was whitee as snow.
+And everywhere that Mary went.
+..........
+CheeseBurgerMacBook-Pro-de-MAikOL:rubyexamples MAikOL$ ruby sa.rb
+Mary had a little lamb.
+Its fleece was whitee as snow.
+And everywhere that Mary went.
+..........
+CheeseBurger
+MacBook-Pro-de-MAikOL:rubyexamples MAikOL$ 
+
+
+
+
+
+
+
+
+
+
+
+
